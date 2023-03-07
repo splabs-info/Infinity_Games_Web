05ecb4aa53049a41f622a95f870670d06f78c1aa
Luu Phuc <luuphuc6297@gmail.com>
2023-03-07 14:22:53 +0700

    update mini games ui

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import { Navigate, useRoutes } from 'react-router-dom';
+import DeepSeaDivers from './games/DeepSeeDivers';
 import ClientLayout from './layouts';
 import ComingSoon from './pages/ComingSoon';
 import Homepage from './pages/Homepage';
@@ -25,7 +26,7 @@ export default function Router() {
                 },
             ],
         },
-        // { path: 'games/deep-sea-divers', element: <DeepSeaDivers /> },
+        { path: 'games/deep-sea-divers', element: <DeepSeaDivers /> },
         { path: '404', element: <NotFound /> },
         { path: 'coming-soon', element: <ComingSoon /> },
         { path: '*', element: <Navigate to="/404" replace /> },
