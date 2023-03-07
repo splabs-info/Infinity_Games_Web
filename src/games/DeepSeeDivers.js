ae4cef07d9e8b24a198c865e2a728b5197217b52
Minh Tam Tran <realbewhy@gmail.com>
2023-03-07 13:44:47 +0700

    update

--- src/games/DeepSeeDivers.js
+++ src/games/DeepSeeDivers.js
@@ -1,10 +1,7 @@
 import React from "react";
 
-import html from "./deepSeaDivers/index.html";
-
-
 export default function DeepSeaDivers() {
   return (
-    <iframe src="/games/deep-sea-divers/index.html" frameborder="0"></iframe>
+    <iframe src="/games/deep-sea-divers/inde∏x.html" frameborder="0"></iframe>
   );
 }
