007719af07b02a06a650e2c00912e02eda201373
Petrus Sĩ Quí <petersiqui@gmail.com>
2023-03-10 15:41:51 +0700

    -- update Background connect wallet

--- src/components/drawer/index.js
+++ src/components/drawer/index.js
@@ -2,6 +2,7 @@ import { Close } from '@mui/icons-material';
 import { Box, Drawer, IconButton } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { ChooseWalletModal, LoggedComponent } from 'components';
+import { Background } from 'constant/styled';
 import { WalletContext } from 'hooks/use-connect';
 import { useContext } from 'react';
 
@@ -9,11 +10,12 @@ const CustomDrawer = styled(Drawer)(({ theme }) => ({
     zIndex: '9998!important',
     ' .MuiDrawer-paperAnchorRight': {
         color: '#fff',
-        background:
-            'linear-gradient(45deg, rgba(17, 173, 169) 0%, rgba(37, 202, 198) 48.96%, rgba(42, 216, 211) 100%)',
+        background: 'url(/images/background/bg-1.jpg)',
+        // 'linear-gradient(45deg, rgba(17, 173, 169) 0%, rgba(37, 202, 198) 48.96%, rgba(42, 216, 211) 100%)',
         backdropFilter: 'blur(10px)',
         width: '420px !important',
         boxShadow: 'none',
+        borderLeft: '1px solid #0A3042'
     },
     [theme.breakpoints.down('sm')]: {
         ' .MuiDrawer-paperAnchorRight': {
